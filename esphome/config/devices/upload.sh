460dda92a39322eea27a8f3d585f4aed6f07669d
ole.klokkhammer <ole.klokkhammer@outlook.com>
2025-01-05 17:22:26 +0100

    Trying to prevent device reboot on no homeassistant running

--- esphome/config/devices/upload.sh
+++ esphome/config/devices/upload.sh
@@ -4,8 +4,10 @@
 # livingroom-roof: 192.168.10.158
 
 device_name=$1
-device_ip=$1
+device_ip=$2
 
 echo "Compiling the ESPHome configuration"
+echo "$device_name"
+echo "$device_ip"
 esphome compile "$device_name".yaml
 esphome upload "$device_name".yaml --device "$device_ip"
