5336677014d66ba16851e0989996b43c9e2e9ed6
ole.klokkhammer <ole.klokkhammer@outlook.com>
2025-06-05 19:57:18 +0200

    New devices

--- esphome/config/upload.sh
+++ esphome/config/upload.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-source venv/bin/activate && cd devices
+source venv/bin/activate  
 
 
 # entrance-roof: 192.168.10.159
 # livingroom-roof: 192.168.10.158
+# dining-room: /dev/ttyUSB0
+# kitchen-roof: /dev/ttyUSB0
 
 device_name=$1
-device_ip=$2
+device_address=$2
 
 echo "Compiling the ESPHome configuration"
 echo "$device_name"
-echo "$device_ip"
+echo "$device_address"
 
-esphome compile "$device_name".yaml
-esphome upload "$device_name".yaml --device "$device_ip"
+esphome compile ./devices/"$device_name".yaml
+esphome upload ./devices/"$device_name".yaml --device "$device_address"
---
+++ esphome/uart/README.md
@@ -0,0 +1,5 @@
+# uart
+
+* install minicom
+* do it with esphome
+* https://community.home-assistant.io/t/shelly-plus-1-flashing-and-setup-guide-tutorial/490396
